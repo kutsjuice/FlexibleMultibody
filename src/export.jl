45aea679c91194d88f835ae0c7225f0f520865a8
Michael Kuts <kutsjuice@gmail.com>
2025-11-19 20:21:26 +0300

    fix(export): fixed exported dictionary collision

--- src/export.jl
+++ src/export.jl
@@ -1,9 +1,10 @@
-function write_solution_to_vtk(comp::FlexibleComponent, filename::String, solutions::Dict{String, Vector{Float64}})
+function write_solution_to_vtk(comp::FlexibleComponent, filename::String, solutions::Vector{Pair{String, Vector{Float64}}})
     #TODO: generalize for additional FEFunction fields (σ(mode_shape), etc.)
     trian = Triangulation(comp.model)
     cellfields = Dict{String, Any}()
-    for (name, sol) in solutions
-        mode_shape = FEFunction(comp.trial, comp.mT*sol)
+    for (i,sol) in enumerate(solutions)
+        name = "sol #$i - $(sol[1])"
+        mode_shape = FEFunction(comp.trial, comp.mT*sol[2])
         cellfields[name] = mode_shape
     end
     writevtk(trian, filename; cellfields)
